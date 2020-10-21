b6517cd833c00cd6ac03f81d4d9d119ea098a6ae
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-10-21 15:13:45 -0300

    update no css e no js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-    return <button className="square">
+    return <button className="square" onClick={props.onClick}>
         {props.value}
     </button>;
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i}/>
+        return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>
     }
 
     render() {
@@ -35,11 +35,63 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null),
+            nextMove: "X"
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares;
+        const move = squares[i];
+
+        // Se o move for diferente de null, significa que tem uma informação lá
+        // Por tanto, devemos encerrar o clique imediatamente, visto que não podemos sobrescrever a informação
+        // A linha return faz exatamente isso, ela encerra a função handleClick
+        // Como qualquer texto, como "X" e "O" são truthy, ou seja, o JS entende como 'true'
+        //      E o if só entra quando existe uma informação 'truthy', esse if será executado
+        //      para qualquer informação válida, no caso, "X" e "O"
+        if (move) {
+            console.log('Square já utilizado', move);
+            return;
+        }
+
+        console.log('handleClick - Game', i);
+
+        squares[i] = 'X';
+
+        this.setState(
+            {
+                squares: squares
+            }
+        )
+
+        /*let move = "";
+        let nextMove = "";
+        
+        if (this.state.nextMove == "X") {
+            move = "X";
+            nextMove = "O";
+        } else {
+            move = "O";
+            nextMove = "X";
+        }
+        this.setState(
+            {
+                move: move,
+                nextMove: nextMove
+            }
+        )*/
+    }
+
     render() {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board/>
+                    <Board squares={this.state.squares} onClick={(i) => this.handleClick(i)}/>
                 </div>
 
                 <div className="game-info">
